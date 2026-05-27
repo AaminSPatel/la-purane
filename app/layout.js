d495c88be5f945f82162e09fb09f1f43f31f907f
AaminSPatel <locomail112@gmail.com>
2026-05-27 17:12:13 +0530

    trying

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 
 import { CartProvider } from '../contexts/CartContext';
 
-const geistSans = Geist({
+const geistSans = null;
+const geistMono = null;
 
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "La Purane",
@@ -23,7 +16,8 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className="h-full antialiased"
+
     >
       <body className="min-h-full flex flex-col">
         <CartProvider>{children}</CartProvider>
